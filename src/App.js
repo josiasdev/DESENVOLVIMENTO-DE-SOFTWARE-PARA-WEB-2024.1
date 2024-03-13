5d3a2c6736955bfdc3fdcc04aabe234387940542
josiasdev <josiasmartins098@gmail.com>
2024-03-13 14:30:07 -0300

    subindo conteúdo novo da aula de web

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-//import HelloWorld from "./components/01HelloWorld/HelloWorld";
+//import HelloWorld from "./components/01_HelloWorld/helloworld";
 //import FuncoesInternas from "./components/02FuncoesInternas/FuncoesInternas";
 //import Galaxia from "./components/04Children/Galaxia";
 //import ComPai from "./components/05ComPaiFilho/ComPai";
 //import Contador from "./components/06Estados/Contador";
-import Pokemon from "./components/06Estados/Pokemon";
+//import Pokemon from "./components/06Estados/Pokemon";
+//import { CompA } from  "./components/07PropDrilling/PropDrillingV2"
+//import { CompA } from "./components/08Context/CompContexto";
+import PokemonPrincipal from "./components/09PokemonContexto/PokemonPrincipal";
 
 function App() {
   return (
     <div>
-      <h1>Desenvolvimento de Software para WEB!</h1>
-      <h3>Prof. Jefferson de Carvalho</h3>
-      <Pokemon />
+      <PokemonPrincipal />
     </div>
   );
 }
