28e5b5364a55dc9a0aa530d89e6f3632ad817c06
josiasdev <josiasmartins098@gmail.com>
2024-07-24 13:27:09 -0300

    Atualizando App js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 //import HelloWorld from "./components/01_HelloWorld/helloworld";
 //import FuncoesInternas from "./components/02FuncoesInternas/FuncoesInternas";
 //import Galaxia from "./components/04Children/Galaxia";
+//import Planeta from "./components/04Children/Planeta";
 //import ComPai from "./components/05ComPaiFilho/ComPai";
 //import Contador from "./components/06Estados/Contador";
 //import Pokemon from "./components/06Estados/Pokemon";
 //import { CompA } from  "./components/07PropDrilling/PropDrillingV2"
 //import { CompA } from "./components/08Context/CompContexto";
 //import PokemonPrincipal from "./components/09PokemonContexto/PokemonPrincipal";
-// import Pai from "./components/atividade01/questao01/01Pai";
-// import * as PC from './components/atividade01/02MeuPC';
-// import World from './components/atividade01/World';
-// import Arena from './components/atividade01/Arena';
-// import { Hero, Enemy } from './components/atividade01/03Batalha';
-// import goku from './components/atividade01/goku.png';
-// import gohan from './components/atividade01/gohan.png';
-import FunctionContextA from './components/atividade02/01Color/FunctionContextA'
-import ComponenteAvo from './components/atividade02/02Pokemon/ComponenteAvo'
-import ComponenteAvoContador from './components/atividade02/03Contador/ComponenteAvo'
+//import Pai from "./components/atividade01/questao01/01Pai";
+//import * as PC from './components/atividade01/02MeuPC';
+//import World from './components/atividade01/World';
+//import Arena from './components/atividade01/Arena';
+//import { Hero, Enemy } from './components/atividade01/03Batalha';
+//import goku from './components/atividade01/goku.png';
+//import gohan from './components/atividade01/gohan.png';
+//import FunctionContextA from './components/atividade02/01Color/FunctionContextA'
+//import ComponenteAvo from './components/atividade02/02Pokemon/ComponenteAvo'
+//import ComponenteAvoContador from './components/atividade02/03Contador/ComponenteAvo'
+//import AcessarCVD19 from "./components/10Axios/AcessarCVD19";
+//import SaveLoadData from "./components/12LocalSessionStorage/SaveLoadData";
+//import MyNavbar from "./components/14Bootstrap/MyNavBar";
+import Main from "./components/15CRUD/Main";
+
 function App() {
   return (
     <div>
-      <FunctionContextA/>
-      <hr/>
-      <ComponenteAvo/>
-      <hr/>
-
-      <ComponenteAvoContador/>
+      <Main />
     </div>
-  );
+  )
 }
 
 export default App;
